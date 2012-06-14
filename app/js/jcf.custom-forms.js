8a0ae8e8fa3da3f5f4271c5be4cb11c995dbf701
James Miller <bensie@gmail.com>
2012-06-14 16:59:06 -0700

    Initial commit

---
+++ app/js/jcf.custom-forms.js
@@ -0,0 +1,519 @@
+// custom radio module
+jcf.addModule({
+	name:'radio',
+	selector: 'input[type="radio"]',
+	defaultOptions: {
+		wrapperClass:'rad-area',
+		focusClass:'rad-focus',
+		checkedClass:'rad-checked',
+		uncheckedClass:'rad-unchecked',
+		disabledClass:'rad-disabled',
+		radStructure:'<span></span>'
+	},
+	getRadioGroup: function(item){
+		var name = item.getAttribute('name');
+		if(name) {
+			return jcf.lib.queryBySelector('input[name="'+name+'"]', jcf.lib.getParent('form'));
+		} else {
+			return [item];
+		}
+	},
+	setupWrapper: function(){
+		jcf.lib.addClass(this.fakeElement, this.options.wrapperClass);
+		this.fakeElement.innerHTML = this.options.radStructure;
+		this.realElement.parentNode.insertBefore(this.fakeElement, this.realElement);
+		this.refreshState();
+		this.addEvents();
+	},
+	addEvents: function(){
+		jcf.lib.event.add(this.fakeElement, 'click', this.toggleRadio, this);
+		if(this.labelFor) {
+			jcf.lib.event.add(this.labelFor, 'click', this.toggleRadio, this);
+		}
+	},
+	onFocus: function(e) {
+		jcf.modules[this.name].superclass.onFocus.apply(this, arguments);
+		setTimeout(jcf.lib.bind(function(){
+			this.refreshState();
+		},this),10);
+	},
+	toggleRadio: function(){
+		if(!this.realElement.disabled) {
+			this.realElement.checked = true;
+		}
+		this.refreshState();
+	},
+	refreshState: function(){
+		var els = this.getRadioGroup(this.realElement);
+		for(var i = 0; i < els.length; i++) {
+			var curEl = els[i].jcf;
+			if(curEl) {
+				if(curEl.realElement.checked) {
+					jcf.lib.addClass(curEl.fakeElement, curEl.options.checkedClass);
+					jcf.lib.removeClass(curEl.fakeElement, curEl.options.uncheckedClass);
+					if(curEl.labelFor) {
+						jcf.lib.addClass(curEl.labelFor, curEl.options.labelActiveClass);
+					}
+				} else {
+					jcf.lib.removeClass(curEl.fakeElement, curEl.options.checkedClass);
+					jcf.lib.addClass(curEl.fakeElement, curEl.options.uncheckedClass);
+					if(curEl.labelFor) {
+						jcf.lib.removeClass(curEl.labelFor, curEl.options.labelActiveClass);
+					}
+				}
+				if(curEl.realElement.disabled) {
+					jcf.lib.addClass(curEl.fakeElement, curEl.options.disabledClass);
+					if(curEl.labelFor) {
+						jcf.lib.addClass(curEl.labelFor, curEl.options.labelDisabledClass);
+					}
+				} else {
+					jcf.lib.removeClass(curEl.fakeElement, curEl.options.disabledClass);
+					if(curEl.labelFor) {
+						jcf.lib.removeClass(curEl.labelFor, curEl.options.labelDisabledClass);
+					}
+				}
+			}
+		}
+	}
+});
+
+// custom checkbox module
+jcf.addModule({
+	name:'checkbox',
+	selector:'input[type="checkbox"]',
+	defaultOptions: {
+		wrapperClass:'chk-area',
+		focusClass:'chk-focus',
+		checkedClass:'chk-checked',
+		labelActiveClass:'chk-label-active',
+		uncheckedClass:'chk-unchecked',
+		disabledClass:'chk-disabled',
+		chkStructure:'<span></span>'
+	},
+	setupWrapper: function(){
+		jcf.lib.addClass(this.fakeElement, this.options.wrapperClass);
+		this.fakeElement.innerHTML = this.options.chkStructure;
+		this.realElement.parentNode.insertBefore(this.fakeElement, this.realElement);
+		jcf.lib.event.add(this.realElement, 'click', this.onRealClick, this);
+		this.refreshState();
+	},
+	onFakePressed: function() {
+		jcf.modules[this.name].superclass.onFakePressed.apply(this, arguments);
+		this.realElement.focus();
+	},
+	onFakeClick: function(e) {
+		jcf.modules[this.name].superclass.onFakeClick.apply(this, arguments);
+		this.tmpTimer = setTimeout(jcf.lib.bind(function(){
+			this.toggle();
+		},this),10);
+		return false;
+	},
+	onRealClick: function(e) {
+		setTimeout(jcf.lib.bind(function(){
+			this.refreshState();
+		},this),10);
+		e.stopPropagation();
+	},
+	toggle: function(e){
+		if(!this.realElement.disabled) {
+			if(this.realElement.checked) {
+				this.realElement.checked = false;
+			} else {
+				this.realElement.checked = true;
+			}
+		}
+		this.refreshState();
+		return false;
+	},
+	refreshState: function(){
+		if(this.realElement.checked) {
+			jcf.lib.addClass(this.fakeElement, this.options.checkedClass);
+			jcf.lib.removeClass(this.fakeElement, this.options.uncheckedClass);
+			if(this.labelFor) {
+				jcf.lib.addClass(this.labelFor, this.options.labelActiveClass);
+			}
+		} else {
+			jcf.lib.removeClass(this.fakeElement, this.options.checkedClass);
+			jcf.lib.addClass(this.fakeElement, this.options.uncheckedClass);
+			if(this.labelFor) {
+				jcf.lib.removeClass(this.labelFor, this.options.labelActiveClass);
+			}
+		}
+		if(this.realElement.disabled) {
+			jcf.lib.addClass(this.fakeElement, this.options.disabledClass);
+			if(this.labelFor) {
+				jcf.lib.addClass(this.labelFor, this.options.labelDisabledClass);
+			}
+		} else {
+			jcf.lib.removeClass(this.fakeElement, this.options.disabledClass);
+			if(this.labelFor) {
+				jcf.lib.removeClass(this.labelFor, this.options.labelDisabledClass);
+			}
+		}
+	}
+});
+
+
+// custom select module
+jcf.addModule({
+	name:'select',
+	selector:'select',
+	defaultOptions: {
+		handleDropPosition: true,
+		wrapperClass:'select-area',
+		focusClass:'select-focus',
+		dropActiveClass:'select-active',
+		selectedClass:'item-selected',
+		disabledClass:'select-disabled',
+		valueSelector:'span.center',
+		optGroupClass:'optgroup',
+		openerSelector:'a.select-opener',
+		selectStructure:'<span class="left"></span><span class="center"></span><a class="select-opener"></a>',
+		selectPrefixClass:'select-',
+		dropMaxHeight: 200,
+		dropFlippedClass: 'select-options-flipped',
+		dropHiddenClass:'options-hidden',
+		dropScrollableClass:'options-overflow',
+		dropClass:'select-options',
+		dropClassPrefix:'drop-',
+		dropStructure:'<div class="drop-holder"><div class="drop-list"></div></div>',
+		dropSelector:'div.drop-list'
+	},
+	checkElement: function(el){
+		return (!el.size && !el.multiple);
+	},
+	setupWrapper: function(){
+		jcf.lib.addClass(this.fakeElement, this.options.wrapperClass);
+		this.realElement.parentNode.insertBefore(this.fakeElement, this.realElement);
+		this.fakeElement.innerHTML = this.options.selectStructure;
+		this.fakeElement.style.width = (this.realElement.offsetWidth > 0 ? this.realElement.offsetWidth + 'px' : 'auto');
+		jcf.lib.addClass(this.fakeElement, jcf.lib.getAllClasses(this.realElement.className, this.options.selectPrefixClass, jcf.baseOptions.hiddenClass));
+
+		// create select body
+		this.opener = jcf.lib.queryBySelector(this.options.openerSelector, this.fakeElement)[0];
+		this.valueText = jcf.lib.queryBySelector(this.options.valueSelector, this.fakeElement)[0];
+		this.opener.jcf = this;
+
+		this.createDropdown();
+		this.refreshState();
+		this.addEvents();
+		this.onControlReady(this);
+		this.hideDropdown(true);
+	},
+	addEvents: function(){
+		jcf.lib.event.add(this.fakeElement, 'click', this.toggleDropdown, this);
+		jcf.lib.event.add(this.realElement, 'change', this.onChange, this);
+	},
+	onFakeClick: function() {
+		// do nothing (drop toggles by toggleDropdown method)
+	},
+	onFocus: function(){
+		// Mac Safari Fix
+		if(jcf.lib.browser.safariMac) {
+			this.realElement.setAttribute('size','2');
+		}
+		jcf.modules[this.name].superclass.onFocus.apply(this, arguments);
+		jcf.lib.event.add(this.realElement, 'keydown', this.onKeyDown, this);
+		if(jcf.activeControl && jcf.activeControl != this) {
+			jcf.activeControl.hideDropdown();
+			jcf.activeControl = this;
+		}
+	},
+	onBlur: function(){
+		// Mac Safari Fix
+		if(jcf.lib.browser.safariMac) {
+			this.realElement.removeAttribute('size');
+		}
+		if(!this.isActiveDrop() || !this.isOverDrop()) {
+			jcf.modules[this.name].superclass.onBlur.apply(this);
+			if(jcf.activeControl === this) jcf.activeControl = null;
+			if(!jcf.isTouchDevice) {
+				this.hideDropdown();
+			}
+		}
+		jcf.lib.event.remove(this.realElement, 'keydown', this.onKeyDown);
+	},
+	onChange: function() {
+		this.refreshState();
+	},
+	onKeyDown: function(e){
+		jcf.tmpFlag = true;
+		setTimeout(function(){jcf.tmpFlag = false},100);
+		var context = this;
+		context.keyboardFix = true;
+		setTimeout(function(){
+			context.refreshState();
+		},10);
+		if(e.keyCode == 13) {
+			context.toggleDropdown.apply(context);
+			return false;
+		}
+	},
+	onResizeWindow: function(e){
+		if(this.isActiveDrop()) {
+			this.hideDropdown();
+		}
+	},
+	onScrollWindow: function(e){
+		if(this.isActiveDrop()) {
+			this.positionDropdown();
+		}
+	},
+	onOptionClick: function(e){
+		var opener = e.target && e.target.tagName && e.target.tagName.toLowerCase() == 'li' ? e.target : jcf.lib.getParent(e.target, 'li');
+		if(opener) {
+			this.realElement.selectedIndex = parseInt(opener.getAttribute('rel'));
+			if(jcf.isTouchDevice) {
+				this.onFocus();
+			} else {
+				this.realElement.focus();
+			}
+			this.refreshState();
+			this.hideDropdown();
+			jcf.lib.fireEvent(this.realElement, 'change');
+		}
+		return false;
+	},
+	onClickOutside: function(e){
+		if(jcf.tmpFlag) {
+			jcf.tmpFlag = false;
+			return;
+		}
+		if(!jcf.lib.isParent(this.fakeElement, e.target) && !jcf.lib.isParent(this.selectDrop, e.target)) {
+			this.hideDropdown();
+		}
+	},
+	onDropHover: function(e){
+		if(!this.keyboardFix) {
+			this.hoverFlag = true;
+			var opener = e.target && e.target.tagName && e.target.tagName.toLowerCase() == 'li' ? e.target : jcf.lib.getParent(e.target, 'li');
+			if(opener) {
+				this.realElement.selectedIndex = parseInt(opener.getAttribute('rel'));
+				this.refreshSelectedClass(parseInt(opener.getAttribute('rel')));
+			}
+		} else {
+			this.keyboardFix = false;
+		}
+	},
+	onDropLeave: function(){
+		this.hoverFlag = false;
+	},
+	isActiveDrop: function(){
+		return !jcf.lib.hasClass(this.selectDrop, this.options.dropHiddenClass);
+	},
+	isOverDrop: function(){
+		return this.hoverFlag;
+	},
+	createDropdown: function(){
+		// remove old dropdown if exists
+		if(this.selectDrop) {
+			this.selectDrop.parentNode.removeChild(this.selectDrop);
+		}
+
+		// create dropdown holder
+		this.selectDrop = document.createElement('div');
+		this.selectDrop.className = this.options.dropClass;
+		this.selectDrop.innerHTML = this.options.dropStructure;
+		jcf.lib.setStyles(this.selectDrop, {position:'absolute'});
+		this.selectList = jcf.lib.queryBySelector(this.options.dropSelector,this.selectDrop)[0];
+		jcf.lib.addClass(this.selectDrop, this.options.dropHiddenClass);
+		document.body.appendChild(this.selectDrop);
+		this.selectDrop.jcf = this;
+		jcf.lib.event.add(this.selectDrop, 'click', this.onOptionClick, this);
+		jcf.lib.event.add(this.selectDrop, 'mouseover', this.onDropHover, this);
+		jcf.lib.event.add(this.selectDrop, 'mouseout', this.onDropLeave, this);
+		this.buildDropdown();
+	},
+	buildDropdown: function() {
+		// build select options / optgroups
+		this.buildDropdownOptions();
+
+		// position and resize dropdown
+		this.positionDropdown();
+
+		// cut dropdown if height exceedes
+		this.buildDropdownScroll();
+	},
+	buildDropdownOptions: function() {
+		this.resStructure = '';
+		this.optNum = 0;
+		for(var i = 0; i < this.realElement.children.length; i++) {
+			this.resStructure += this.buildElement(this.realElement.children[i]) +'\n';
+		}
+		this.selectList.innerHTML = this.resStructure;
+	},
+	buildDropdownScroll: function() {
+		if(this.options.dropMaxHeight) {
+			if(this.selectDrop.offsetHeight > this.options.dropMaxHeight) {
+				this.selectList.style.height = this.options.dropMaxHeight+'px';
+				this.selectList.style.overflow = 'auto';
+				this.selectList.style.overflowX = 'hidden';
+				jcf.lib.addClass(this.selectDrop, this.options.dropScrollableClass);
+			}
+		}
+		jcf.lib.addClass(this.selectDrop, jcf.lib.getAllClasses(this.realElement.className, this.options.dropClassPrefix, jcf.baseOptions.hiddenClass));
+	},
+	buildElement: function(obj){
+		// build option
+		var res = '';
+		if(obj.tagName.toLowerCase() == 'option') {
+			if(!jcf.lib.prevSibling(obj) || jcf.lib.prevSibling(obj).tagName.toLowerCase() != 'option') {
+				res += '<ul>';
+			}
+			res += '<li rel="'+(this.optNum++)+'" class="'+(obj.className? obj.className : '')+' jcfcalc"><a href="#">'+(obj.title? '<img src="'+obj.title+'" alt="" />' : '')+'<span>' + obj.innerHTML + '</span></a></li>';
+			if(!jcf.lib.nextSibling(obj) || jcf.lib.nextSibling(obj).tagName.toLowerCase() != 'option') {
+				res += '</ul>';
+			}
+			return res;
+		}
+		// build option group with options
+		else if(obj.tagName.toLowerCase() == 'optgroup' && obj.label) {
+			res += '<div class="'+this.options.optGroupClass+'">';
+			res += '<strong class="jcfcalc"><em>'+(obj.label)+'</em></strong>';
+			for(var i = 0; i < obj.children.length; i++) {
+				res += this.buildElement(obj.children[i]);
+			}
+			res += '</div>';
+			return res;
+		}
+	},
+	positionDropdown: function(){
+		var ofs = jcf.lib.getOffset(this.fakeElement), selectAreaHeight = this.fakeElement.offsetHeight, selectDropHeight = this.selectDrop.offsetHeight;
+		if(this.options.handleDropPosition && jcf.lib.getScrollTop() + jcf.lib.getWindowHeight() < ofs.top + selectAreaHeight + selectDropHeight) {
+			this.selectDrop.style.top = (ofs.top - selectDropHeight)+'px';
+			jcf.lib.addClass(this.selectDrop, this.options.dropFlippedClass);
+		} else {
+			this.selectDrop.style.top = (ofs.top + selectAreaHeight)+'px';
+			jcf.lib.removeClass(this.selectDrop, this.options.dropFlippedClass);
+		}
+		this.selectDrop.style.left = ofs.left+'px';
+		this.selectDrop.style.width = this.fakeElement.offsetWidth+'px';
+	},
+	showDropdown: function(){
+		document.body.appendChild(this.selectDrop);
+		jcf.lib.removeClass(this.selectDrop, this.options.dropHiddenClass);
+		jcf.lib.addClass(this.fakeElement,this.options.dropActiveClass);
+		this.positionDropdown();
+
+		// show current dropdown
+		jcf.lib.event.add(window, 'resize', this.onResizeWindow, this);
+		jcf.lib.event.add(window, 'scroll', this.onScrollWindow, this);
+		jcf.lib.event.add(document, jcf.eventPress, this.onClickOutside, this);
+		this.positionDropdown();
+	},
+	hideDropdown: function(partial){
+		if(this.selectDrop.parentNode) {
+			if(this.selectDrop.offsetWidth) {
+				this.selectDrop.parentNode.removeChild(this.selectDrop);
+			}
+			if(partial) {
+				return;
+			}
+		}
+		if(typeof this.origSelectedIndex === 'number') {
+			this.realElement.selectedIndex = this.origSelectedIndex;
+		}
+		jcf.lib.removeClass(this.fakeElement,this.options.dropActiveClass);
+		jcf.lib.addClass(this.selectDrop, this.options.dropHiddenClass);
+		jcf.lib.event.remove(window, 'resize', this.onResizeWindow);
+		jcf.lib.event.remove(window, 'scroll', this.onScrollWindow);
+		jcf.lib.event.remove(document.documentElement, jcf.eventPress, this.onClickOutside);
+		if(jcf.isTouchDevice) {
+			this.onBlur();
+		}
+	},
+	toggleDropdown: function(){
+		if(jcf.isTouchDevice) {
+			this.onFocus();
+		} else {
+			this.realElement.focus();
+		}
+		this.dropOpened = true;
+		if(!this.realElement.disabled) {
+			if(this.isActiveDrop()) {
+				this.hideDropdown();
+			} else {
+				this.showDropdown();
+			}
+		}
+		this.refreshState();
+	},
+	scrollToItem: function(){
+		if(this.isActiveDrop()) {
+			var dropHeight = this.selectList.offsetHeight;
+			var offsetTop = this.calcOptionOffset(this.getFakeActiveOption());
+			var sTop = this.selectList.scrollTop;
+			var oHeight = this.getFakeActiveOption().offsetHeight;
+			//offsetTop+=sTop;
+
+			if(offsetTop >= sTop + dropHeight) {
+				this.selectList.scrollTop = offsetTop - dropHeight + oHeight;
+			} else if(offsetTop < sTop) {
+				this.selectList.scrollTop = offsetTop;
+			}
+		}
+	},
+	getFakeActiveOption: function(c) {
+		return jcf.lib.queryBySelector('li[rel="'+(typeof c === 'number' ? c : this.realElement.selectedIndex) +'"]',this.selectList)[0];
+	},
+	calcOptionOffset: function(fake) {
+		var h = 0;
+		var els = jcf.lib.queryBySelector('.jcfcalc',this.selectList);
+		for(var i = 0; i < els.length; i++) {
+			if(els[i] == fake) break;
+			h+=els[i].offsetHeight;
+		}
+		return h;
+	},
+	childrenHasItem: function(hold,item) {
+		var items = hold.getElementsByTagName('*');
+		for(i = 0; i < items.length; i++) {
+			if(items[i] == item) return true;
+		}
+		return false;
+	},
+	removeSelectedClass: function(){
+		var children = jcf.lib.queryBySelector('li',this.selectList);
+		for(var i = children.length - 1; i >= 0; i--) {
+			jcf.lib.removeClass(children[i], this.options.selectedClass);
+		}
+	},
+	setSelectedClass: function(c){
+		jcf.lib.addClass(this.getFakeActiveOption(c), this.options.selectedClass);
+	},
+	refreshSelectedClass: function(c){
+		this.removeSelectedClass(c);
+		this.setSelectedClass(c);
+		if(this.realElement.disabled) {
+			jcf.lib.addClass(this.fakeElement, this.options.disabledClass);
+			if(this.labelFor) {
+				jcf.lib.addClass(this.labelFor, this.options.labelDisabledClass);
+			}
+		} else {
+			jcf.lib.removeClass(this.fakeElement, this.options.disabledClass);
+			if(this.labelFor) {
+				jcf.lib.removeClass(this.labelFor, this.options.labelDisabledClass);
+			}
+		}
+	},
+	refreshSelectedText: function() {
+		if(!this.dropOpened && this.realElement.title) {
+			this.valueText.innerHTML = this.realElement.title;
+		} else {
+			if(this.realElement.options[this.realElement.selectedIndex].title) {
+				this.valueText.innerHTML = '<img src="'+this.realElement.options[this.realElement.selectedIndex].title+'" alt="" />' + this.realElement.options[this.realElement.selectedIndex].innerHTML;
+			} else {
+				this.valueText.innerHTML = this.realElement.options[this.realElement.selectedIndex].innerHTML;
+			}
+		}
+	},
+	refreshState: function(){
+		this.origSelectedIndex = this.realElement.selectedIndex;
+		this.refreshSelectedClass();
+		this.refreshSelectedText();
+		this.positionDropdown();
+		if(this.selectDrop.offsetWidth) {
+			this.scrollToItem();
+		}
+	}
+});
