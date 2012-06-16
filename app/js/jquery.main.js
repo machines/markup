0937e85f092ed31d23bbe6be77445a7e81e527af
James Miller <bensie@gmail.com>
2012-06-16 09:17:58 -0700

    Fix modal loading via AJAX, remove X close button from modals by default

--- app/js/jquery.main.js
+++ app/js/jquery.main.js
@@ -2,6 +2,18 @@
 jQuery(function(){
   initInputs(); // clear inputs on focus
   initSameHeight(); // set same box height
+  $.modal.defaults = {
+    overlay: "#000",        // Overlay color
+    opacity: 0.75,          // Overlay opacity
+    zIndex: 1,              // Overlay z-index.
+    escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
+    clickClose: true,       // Allows the user to close the modal by clicking the overlay
+    closeText: 'Close',     // Text content for the close <a> tag.
+    showClose: false,        // Shows a (X) icon/link in the top-right corner
+    modalClass: "lightbox",    // CSS class added to the element being displayed in the modal.
+    spinnerHtml: null,      // HTML appended to the default spinner during AJAX requests.
+    showSpinner: true       // Enable/disable the default spinner during AJAX requests.
+  };
 });
 
 
